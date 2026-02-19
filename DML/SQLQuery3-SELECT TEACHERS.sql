c662e66fa247251ec937e02e81b0794ef1cba95f
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-02-19 19:23:54 +0300

    HW1 DDL

--- DML/SQLQuery3-SELECT TEACHERS.sql
+++ DML/SQLQuery3-SELECT TEACHERS.sql
@@ -31,3 +31,13 @@ WHERE	teacher = teacher_id
 AND		discipline = discipline_id
 GROUP BY  last_name, first_name, middle_name 
 ;
+
+SELECT
+		  [Преподаватель]		  =	FORMATMESSAGE(N'%s%s%s',last_name, first_name, middle_name)	
+		  ,[дисциплина] = (discipline_name)
+		 		
+FROM    Teachers, TeachersDisciplinesRelation, Disciplines
+WHERE	teacher = teacher_id
+AND		discipline = discipline_id
+
+;
